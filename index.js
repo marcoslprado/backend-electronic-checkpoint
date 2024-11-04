8adbc98219f31b62be106c1015ea86c9b6126b1a
Marcos <marcos.lprado@sempreceub.com>
2024-11-04 10:40:45 -0300

    feat: creates project following Airton in classes

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express(); // Método construtor //
+
+// app.METODO('rota/caminho', (req, res) => {})
+
+app.get('/users', (req, res) => {
+    res.send("Nessa rota vou retornar os usuários");
+});
+
+app.post('/users', (req, res) => {
+    res.send("Rota users usando post");
+});
+
+app.get('/user/:id', (req, res) => {
+    console.log(`O parâmetro é ${req.params.id}`);
+});
+
+app.post('/user/:id1-:id2', (req, res) => {
+    res.send(req.params);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor web ouvindo na porta ${PORT}`);
+});
